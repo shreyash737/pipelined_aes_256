17903766c96e06e10928bf075b4acc723e56f3d9
Shreyash Jadhav <shreyashjadhav737@gmail.com>
2026-03-22 16:40:40 +0530

    key expansion

--- fpga_modules/key_expansion.v
+++ fpga_modules/key_expansion.v
@@ -1,117 +1,68 @@
-
-
-
-module key_expansion(input [255:0] key_in ,input clk ,input reset_n, output reg [1919:0] expanded_key_out);
-    wire [31:0] key [0:7];
-    reg [31:0] expanded_key [0:59];
-    wire [31:0] s_word[0:59];
- 
-
-    genvar i ;
-
-    generate
-        for (i=0;i < 8;i=i+1) begin
-            assign  key [i] = key_in[((i+1)*32-1):(i*32)]
-
-        end
-    endgenerate
-
-    generate
-        for(i=8;i<60;i=i+1)begin
-            if(i%8==0) begin
-            wire [7:0] rot_wordout;
-            assign rot_wordout = rot_word(expanded_key[i-1])
-
-            s_box instance1(rot_wordout,s_word[i])
-
-            end else if(i%8==4)
-            s_box instance2(expanded_key[i-1],s_word[i])
-        end
-
-    endgenerate
-
-   
-        
+module key_expansion(input [255:0]key,input clk,input reset_n,output reg [1919:0] expandedkey , output reg ready);
+    reg [31:0] internal_key [0:59] ;
+    wire [31:0]sub_word_input;
+    wire [31:0] sub_word_output;
     
 
-   
+    genvar i;
+    generate
+        s_box instance1(.in(sub_word_input[7:0]),.out(sub_word_output[7:0]));
+        s_box instance1(.in(sub_word_input[15:8]),.out(sub_word_output[15:7]));
+        s_box instance1(.in(sub_word_input[23:16]),.out(sub_word_output[23:16]));
+        s_box instance1(.in(sub_word_input[31:24]),.out(sub_word_output[31:24]));
+    endgenerate
 
+    integer counter = 0;
 
-   integer  i=0  ;
-   wire [7:0] s_word;
-   always @(posedge clk negedge rest_n) begin
+    assign sbox_in_word = (count % 8 == 0) ? {w[count-1][23:0], w[count-1][31:24]} : // RotWord
+                          (count % 8 == 4) ? w[count-1]                           : // SubWord only
+                          32'h0;
+
+    always @(posedge clk or negedge reset_n) begin
         if(!reset_n) begin
-            expanded_key_out <= {1920{1'b0}}
+            internal_key <= 0;
+            ready <= 0;
+            expandedkey <= 0;
+            counter <= 0;
         end else begin
+            if(counter<8)begin
+                internal_key = key[(counter*8-1):(counter*8)];
+                counter = counter+1;
+            end
 
-        if(count<8) begin
-           expanded_key[i] <= in[i];
-        end
-           
-        
-
-        if(count<60) begin
-            if (i%8==0) begin
-                expanded_key[i] <= expanded_key[i-8]^rcon(s_word[i],get_rcon(i/8));   
-
-                end else if(i%8==4) begin
-                
-                expanded_key[i] <= expanded_key[i-8]^s_word[i];  
-
+            if(count < 60) begin
+                if(counter%8==0) begin
+                   
+                    internal_key[counter] <= internal_key[counter-8]^sub_word_output^{get_rcon(count/8), 24'h0};
+                end else if(counter%8==4) begin
+                    
+                    internal_key[counter] <= internal_key[counter-8]^sub_word_output;
                 end else begin
-
-                expanded_key[i] <= expanded_key[i-8]^expanded_key[i-1];
+                    internal_key[counter] <= internal_key[counter-8]^internal_key(coun_ter-1)
                 end
-           
+                counter = counter + 1;
+            end else begin
+                ready <= 1'b0;
+                integer k;
+                for (k = 0; k < 60; k = k + 1) begin
+                    expandedkey[k*32 +: 32] <= internal_key[k];
+                end
+
+            end
+
+
         end
+    end
 
-        count <= count+1;
-
-        end
-        
-        
-   end
-
-   generate 
-    for (i=0;i < 60) begin
-            assign  expanded_key_out[((i+1)*32-1):(i*32)] = expanded_key[i];
-        end
-   endgenerate
-
-   function [7:0] get_rcon(input [3:0] round_idx); // Give 'i' a width (4 bits for 14 rounds)
-    begin
+    function [7:0] get_rcon(input [3:0] round_idx);
         case(round_idx)
-            4'd1:  get_rcon = 8'h01;
-            4'd2:  get_rcon = 8'h02;
-            4'd3:  get_rcon = 8'h04;
-            4'd4:  get_rcon = 8'h08;
-            4'd5:  get_rcon = 8'h10;
-            4'd6:  get_rcon = 8'h20;
-            4'd7:  get_rcon = 8'h40;
-            4'd8:  get_rcon = 8'h80;
-            4'd9:  get_rcon = 8'h1b; 
-            4'd10: get_rcon = 8'h36;
-            default: get_rcon = 8'h00; 
+            4'd1:  get_rcon = 8'h01; 4'd2:  get_rcon = 8'h02;
+            4'd3:  get_rcon = 8'h04; 4'd4:  get_rcon = 8'h08;
+            4'd5:  get_rcon = 8'h10; 4'd6:  get_rcon = 8'h20;
+            4'd7:  get_rcon = 8'h40; 4'd8:  get_rcon = 8'h80;
+            4'd9:  get_rcon = 8'h1b; 4'd10: get_rcon = 8'h36;
+            default: get_rcon = 8'h00;
         endcase
-    end
     endfunction
 
-
-     function [31:0] rcon (input [31:0] in , input [7:0] rc);
-    rcon = in ^ {rc,8'h00,8'h00,8'h00}; 
-    endfunction
-
-    function [31:0] rot_word (input [31:0] in );
-     rot_word = {in[23:0],in[31:24]};
-    endfunction
-
-    function [31:0] sub_word (input [31:0] in);
-    begin
-     sub_word[7:0] = s_box(in[7:0]);
-     sub_word[15:8] = s_box(in[15:8]);
-     sub_word[23:16] = s_box(in[23:16]);
-     sub_word[31:24] = s_box(in[31:24]);
-    end
-    endfunction
 endmodule
-
